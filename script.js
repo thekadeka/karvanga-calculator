e5ecb3df9c2ea7bbb7ab316abf3e8b4a9b411809
Kadeka <197521428+kad3ka@users.noreply.github.com>
2025-04-24 01:29:46 +0300

    Change the logic of display and first variable

--- script.js
+++ script.js
@@ -1,107 +1,65 @@
-let isDisplaySum = false;
-
-// Resets display when pressing a new character when sum is on display
-
-// Operator functions
-function add(arr) {
-  sum = arr[0] + arr[2];
-
-  display.textContent = sum;
-  isDisplaySum = true;
-  console.log(isDisplaySum);
+// Operation functions
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(arr) {
-  sum = arr[0] - arr[2];
-  display.textContent = sum;
-  isDisplaySum = true;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(arr) {
-  sum = arr[0] * arr[2];
-  display.textContent = sum;
-  isDisplaySum = true;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(arr) {
-  sum = arr[0] / arr[2];
-  display.textContent = sum;
-  isDisplaySum = true;
+function divide(a, b) {
+  return a / b;
 }
 
-// Populate the display and store the numbers
+// Variables
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
 
-function populate() {
+function operate() {}
+
+function populate(firstNumber, operator, secondNumber) {
   const operators = document.querySelectorAll('.operator');
   const numbers = document.querySelectorAll('.number');
+  let shouldClearDisplay = false;
+  let firstNumberAcquired = false;
 
   numbers.forEach(function (item) {
     item.addEventListener('click', function (e) {
-      if (isDisplaySum && display.textContent.match(/[+\-*/]/)) {
-        isDisplaySum = false;
+      if (firstNumberAcquired === false) {
+        display.textContent += e.target.textContent;
       }
-
-      if (isDisplaySum) {
-        display.textContent = '';
-        isDisplaySum = false;
-      }
-
-      const number = e.target.textContent;
-      display.textContent += `${number}`;
     });
   });
 
   operators.forEach(function (item) {
     item.addEventListener('click', function (e) {
-      const operator = e.target.textContent;
-      display.textContent += `${operator}`;
+      operator = e.target.textContent;
+      firstNumber = display.textContent;
+      console.log(firstNumber);
+      shouldClearDisplay = true;
+      firstNumberAcquired = true;
+
+      numbers.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+          if (shouldClearDisplay === true) {
+            display.textContent = '';
+            shouldClearDisplay = false;
+          }
+          display.textContent += e.target.textContent;
+          secondNumber = display.textContent;
+          console.log(secondNumber);
+        });
+      });
     });
   });
 }
 
 populate();
-
-// Operation
-
-let firstNumber = 0;
-
-let operator = '';
-
-let secondNumber = 0;
-
-const equals = document.querySelector('#equals');
-equals.addEventListener('click', operate);
-
-function operate() {
-  // Create an array with numbers and operators as strings
-  let operationString = display.textContent;
-  let operationArr = operationString.split(/([\+\-\*\/])/);
-  let operationArrNumbers = operationArr.map(function (item) {
-    num = Number(item);
-    if (isNaN(num)) {
-      return item;
-    } else {
-      return num;
-    }
-  });
-
-  console.log(operationArrNumbers);
-  // Right operation function for right operator
-  if (operationArrNumbers[1] === '+') {
-    add(operationArrNumbers);
-  }
-
-  if (operationArrNumbers[1] === '-') {
-    subtract(operationArrNumbers);
-  }
-
-  if (operationArrNumbers[1] === '*') {
-    multiply(operationArrNumbers);
-  }
-  if (operationArrNumbers[1] === '/') {
-    divide(operationArrNumbers);
-  }
-}
 
 // Clear button
 
