cbd762d002e739c32d9e97b7a5094219a54a3094
Kadeka <197521428+kad3ka@users.noreply.github.com>
2025-04-24 16:19:33 +0300

    Make working calculator with variables

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+// Global variables
+const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+const equals = document.querySelector('#equals');
+let shouldClearDisplay = false;
+let firstNumberAcquired = false;
+
 // Operation functions
 function add(a, b) {
-  console.log(a + b);
+  sum = a + b;
+  display.textContent = '';
+  display.textContent = sum;
+  firstNumberAcquired = true;
 }
 
 function subtract(a, b) {
-  return a - b;
+  sum = a - b;
+  display.textContent = '';
+  display.textContent = sum;
+  firstNumberAcquired = true;
 }
 
 function multiply(a, b) {
-  return a * b;
+  sum = a * b;
+  display.textContent = '';
+  display.textContent = sum;
+  firstNumberAcquired = true;
 }
 
 function divide(a, b) {
-  return a / b;
+  sum = a / b;
+  display.textContent = '';
+  display.textContent = sum;
+  firstNumberAcquired = true;
 }
 
 // Variables
@@ -21,26 +40,47 @@ let operator = '';
 let secondNumber = 0;
 
 function operate(firstNumber, operator, secondNumber) {
+  let num1 = Number(firstNumber);
+  let num2 = Number(secondNumber);
+
   if (operator === '+') {
-    add(firstNumber, secondNumber);
+    add(num1, num2);
+  }
+  if (operator === '-') {
+    subtract(num1, num2);
+  }
+  if (operator === '*') {
+    multiply(num1, num2);
+  }
+  if (operator === '/') {
+    divide(num1, num2);
   }
 }
 
-function populate(firstNumber, operator, secondNumber) {
-  const operators = document.querySelectorAll('.operator');
-  const numbers = document.querySelectorAll('.number');
-  const equals = document.querySelector('#equals');
-  let shouldClearDisplay = false;
-  let firstNumberAcquired = false;
+// x Populate the display and the variables
 
+// x.1 Display the firstNumber
+function populate(firstNumber, operator, secondNumber) {
   numbers.forEach(function (item) {
     item.addEventListener('click', function (e) {
       if (firstNumberAcquired === false) {
         display.textContent += e.target.textContent;
+        console.log('numbers1');
+      }
+
+      if (firstNumberAcquired === true) {
+        if (shouldClearDisplay === true) {
+          display.textContent = '';
+          shouldClearDisplay = false;
+        }
+        if (firstNumberAcquired) {
+          display.textContent += e.target.textContent;
+          console.log('numbers2');
+        }
       }
     });
   });
-
+  // x.2 If operator clicked add value to the firstNumber that is displayed and value to the operator
   operators.forEach(function (item) {
     item.addEventListener('click', function (e) {
       operator = e.target.textContent;
@@ -48,19 +88,12 @@ function populate(firstNumber, operator, secondNumber) {
       console.log(firstNumber);
       shouldClearDisplay = true;
       firstNumberAcquired = true;
-
-      numbers.forEach(function (item) {
-        item.addEventListener('click', function (e) {
-          if (shouldClearDisplay === true) {
-            display.textContent = '';
-            shouldClearDisplay = false;
-          }
-          display.textContent += e.target.textContent;
-        });
-      });
     });
+    // x.3  If new number is clicked clear display and add new numbers to the display
+    //BUG: Every time it runs it adds multiple event listeners to the numbers
   });
 
+  // x.4 If equal sign is clicked add value that is displayed to the secondNumber and call operate on these variables
   equals.addEventListener('click', function () {
     secondNumber = display.textContent;
     console.log(secondNumber);
@@ -76,4 +109,7 @@ populate();
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function () {
   display.textContent = '';
+  let firstNumber = 0;
+  let operator = '';
+  let secondNumber = 0;
 });
